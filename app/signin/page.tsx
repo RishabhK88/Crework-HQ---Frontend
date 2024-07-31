97faab559b2a2c451e961b8e66b87b0fc9f60ec6
Rishabh Kumar <rishabhk@Rishabhs-Laptop.local>
2024-07-31 09:31:34 +0530

    Placed Auth Component Correctly
    Signed-off-by: Rishabh Kumar <rishabhk@Rishabhs-Laptop.local>

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import AuthComponent from "@/components/AuthComponent";
 
 export default function SignIn() {
   return (
-    <div className="bg-gradient-to-b from-[#ffffff] to-[#afa3ff] h-screen w-screen flex justify-center items-center">
+    <div className="bg-gradient-to-b from-[#ffffff] to-[#afa3ff] h-screen w-screen flex justify-center mt-32">
       <AuthComponent authType="SignIn" />
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,7 +2,7 @@ import AuthComponent from "@/components/AuthComponent";
 
 export default function SignUp() {
   return (
-    <div className="bg-gradient-to-b from-[#ffffff] to-[#afa3ff] h-screen w-screen flex justify-center items-center">
+    <div className="bg-gradient-to-b from-[#ffffff] to-[#afa3ff] h-screen w-screen flex justify-center mt-32">
       <AuthComponent authType="SignUp" />
     </div>
   );
